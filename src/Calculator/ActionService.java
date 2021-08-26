ae6e73fe0558363af657ade3d40e5f4e6d9cef65
VladimirMatveyev <89550530+VladimirMatveyev@users.noreply.github.com>
2021-08-27 00:09:10 +0300

    Update ActionService.java

--- src/Calculator/ActionService.java
+++ src/Calculator/ActionService.java
@@ -1,6 +1,6 @@
 package Calculator;
 
-public class ActionService {//proverich
+public class ActionService {
     public static String calculate(Number first, Number second, String action) throws Exception {
 
         int result;
